fdc7ac0702e1d490fe6cf078531297746defdd49
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2018-02-23 17:52:39 +0100

    the more the merrier

--- HNLNtuple/test/gen_ntuplizer.py
+++ HNLNtuple/test/gen_ntuplizer.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 from DataFormats.FWLite import Events, Handle
 from PhysicsTools.HeppyCore.utils.deltar import deltaR, deltaPhi, inConeCollection, bestMatch
 from tree_variables import branches # here the ntuple branches are defined
+from PhysicsTools.Heppy.physicsobjects.GenParticle import GenParticle
 from PhysicsTools.Heppy.physicsobjects.Muon import Muon
 from PhysicsTools.Heppy.physicsobjects.Electron import Electron
 from PhysicsTools.Heppy.physicsobjects.Photon import Photon
@@ -27,8 +28,8 @@ tofill_gen = OrderedDict(zip(branches, [-99.]*len(branches))) # initialise all b
 ##########################################################################################
 # Get ahold of the events
 print '... loading files'
-# events = Events(samples[:1]) # make sure this corresponds to your file name!
-events = Events(samples) # make sure this corresponds to your file name!
+events = Events(samples[:2]) # make sure this corresponds to your file name!
+# events = Events(samples) # make sure this corresponds to your file name!
 print 'done ...'
 maxevents = -1 # max events to process
 totevents = events.size() # total number of events in the files
@@ -49,6 +50,13 @@ handles['pvs'        ] = ( ('offlineSlimmedPrimaryVertices', '', 'PAT' ), Handle
 handles['svs'        ] = ( ('slimmedSecondaryVertices'     , '', 'PAT' ), Handle('std::vector<reco::VertexCompositePtrCandidate>') )
 handles['dsmuons'    ] = ( ('displacedStandAloneMuons'     , '', 'RECO'), Handle('std::vector<reco::Track>'                      ) )
 handles['dgmuons'    ] = ( ('displacedGlobalMuons'         , '', 'RECO'), Handle('std::vector<reco::Track>'                      ) )
+handles['beamspot'   ] = ( ('offlineBeamSpot'              , '', 'RECO'), Handle('reco::BeamSpot'                                ) )
+
+##########################################################################################
+# stuff I need to instantiate only once
+vtxfit = VertexFitter()
+# create a std::vector<RecoChargedCandidate> to be passed to the fitter 
+tofit = ROOT.std.vector('reco::RecoChargedCandidate')()
 
 ##########################################################################################
 # start looping on the events
@@ -87,12 +95,12 @@ for i, ev in enumerate(events):
     # all W bosons
     try:
         the_w = [ip for ip in ev.genp_pruned if abs(ip.pdgId())==24 and ip.isLastCopy() and 9900012 in [ip.daughter(jj).pdgId() for jj in range(ip.numberOfDaughters())]][0]
+        # prompt lepton
+        the_pl = [the_w.daughter(jj) for jj in range(the_w.numberOfDaughters()) if abs(the_w.daughter(jj).pdgId()) in [11,13]][0]
     except:
-        print 'no W, must be off-shell. Passing on'
-        continue
-
-    # prompt lepton
-    the_pl = [the_w.daughter(jj) for jj in range(the_w.numberOfDaughters()) if abs(the_w.daughter(jj).pdgId()) in [11,13]][0]
+        print 'no W, must be off-shell. Picking the prompt lepton from somewhere else'
+        # prompt lepton
+        the_pl = map(GenParticle, [ip for ip in ev.genp_pruned if abs(ip.pdgId()) in [11,13] and ip.isPromptFinalState() and not isAncestor(the_hn, ip)])[0]      
 
     # get the immediate daughters of the heavy neutrino decay
     the_hn.initialdaus = [the_hn.daughter(jj) for jj in range(the_hn.numberOfDaughters())]
@@ -120,7 +128,10 @@ for i, ev in enumerate(events):
         ip.finaldaughters = sorted(finaldaus , key = lambda x : x.pt(), reverse = True)
         ip.hasConvOrRad = (len(ip.finaldaughters)>1)
         if len(ip.finaldaughters)>1:
-            ip.finallep = max([ii for ii in ip.finaldaughters if ii.pdgId()==ip.pdgId()], key = lambda x : x.pt())
+            try:
+                ip.finallep = max([ii for ii in ip.finaldaughters if ii.pdgId()==ip.pdgId()], key = lambda x : x.pt())
+            except:
+                ip.finallep = max([ii for ii in ip.finaldaughters if abs(ii.pdgId()) in [11, 13]], key = lambda x : x.pt())                
         else:
             ip.finallep = ip
 
@@ -155,7 +166,7 @@ for i, ev in enumerate(events):
     for ip in [the_hn.lep1.finallep, the_hn.lep2.finallep, the_pl.finallep]:
         ip.bestmatch     = None
         ip.bestmatchtype = None
-        ip.matches = inConeCollection(ip, matchable, 0.15, 0.)
+        ip.matches = inConeCollection(ip, matchable, 0.2, 0.)
         # to find the best match, give precedence to any matched 
         # particle in the matching cone with the correct PDG ID
         # then to the one which is closest
@@ -164,6 +175,7 @@ for i, ev in enumerate(events):
             ip.bestmatch = ip.matches[0]
             # remove already matched particles, avoid multiple matches to the same candidate
             matchable.remove(ip.bestmatch)
+            # record which is which
             if ip.bestmatch in ev.electrons: ip.bestmatchtype = 0
             if ip.bestmatch in ev.photons  : ip.bestmatchtype = 1
             if ip.bestmatch in ev.muons    : ip.bestmatchtype = 2
@@ -171,10 +183,13 @@ for i, ev in enumerate(events):
             if ip.bestmatch in ev.dsmuons  : ip.bestmatchtype = 4
             if ip.bestmatch in ev.dgmuons  : ip.bestmatchtype = 5
     
+    # clear it before doing it again
+    ev.recoSv = None
+
     # let's refit the secondary vertex, IF both leptons match to some reco particle
-    if the_hn.lep1.finallep.bestmatch and the_hn.lep2.finallep.bestmatch:
-        # create a std::vector<RecoChargedCandidate> to be passed to the fitter 
-        tofit = ROOT.std.vector('reco::RecoChargedCandidate')()
+    if not(the_hn.lep1.finallep.bestmatch is None or the_hn.lep2.finallep.bestmatch is None):
+        # clear the vector
+        tofit.clear()
         # create a RecoChargedCandidate for each reconstructed lepton and flush it into the vector
         for il in [the_hn.lep1.finallep.bestmatch, the_hn.lep2.finallep.bestmatch]:
             # if the reco particle is a displaced thing, it does not have the p4() method, so let's build it 
@@ -183,23 +198,23 @@ for i, ev in enumerate(events):
             ic.setCharge(il.charge())             # assign the correct charge
             ic.setP4(myp4)                        # assign the correct p4
             ic.setTrack(il.track())               # set the correct TrackRef
-            if ic.track().isNonnull():              # check that the track is valid, there are photons around too!
+            if ic.track().isNonnull():            # check that the track is valid, there are photons around too!
                 tofit.push_back(ic)
 
         # further sanity check: two *distinct* tracks
         if tofit.size()==2 and tofit[0].track() != tofit[1].track():
             # fit it!
-            vtxfit = VertexFitter()    # not sure one needs to re-instantiate this every time
             svtree = vtxfit.Fit(tofit) # actual vertex fitting
-            ev.recoSv = None
             # check that the vertex is good
             if not svtree.get().isEmpty() and svtree.get().isValid():
                 svtree.movePointerToTheTop()
                 sv = svtree.currentDecayVertex().get()
                 ev.recoSv = makeRecoVertex(sv, kinVtxTrkSize=2) # need to do some gymastics
         
+#     if ev.eventAuxiliary().event() in [159, 163, 160, 161, 166, 165, 157, 162, 169, 170, 168, 172, 180, 176, 178, 181, 182, 188, 185, 190, 191, 186, 192]:
+#         if the_hn.lep1.finallep.bestmatch is None:
 #             import pdb ; pdb.set_trace()
-
+    
     ######################################################################################
     # fill the ntuple: each gen tau makes an entry
     for k, v in tofill_gen.iteritems(): tofill_gen[k] = -99. # initialise before filling
@@ -296,15 +311,31 @@ for i, ev in enumerate(events):
     tofill_gen['nu_q'        ] = the_hn.neu.charge()
     tofill_gen['nu_pdgid'    ] = the_hn.neu.pdgId()
 
+    tofill_gen['pv_x'        ] = the_hn.vx()
+    tofill_gen['pv_y'        ] = the_hn.vy()
+    tofill_gen['pv_z'        ] = the_hn.vz()
+
     tofill_gen['sv_x'        ] = the_sv.x()
     tofill_gen['sv_y'        ] = the_sv.y()
     tofill_gen['sv_z'        ] = the_sv.z()
+
+    if len(ev.pvs)>0:
+        tofill_gen['pv_reco_x'] = ev.pvs[0].x()
+        tofill_gen['pv_reco_y'] = ev.pvs[0].y()
+        tofill_gen['pv_reco_z'] = ev.pvs[0].z()
 
     if hasattr(ev, 'recoSv') and ev.recoSv:
         tofill_gen['sv_reco_x'   ] = ev.recoSv.x()
         tofill_gen['sv_reco_y'   ] = ev.recoSv.y()
         tofill_gen['sv_reco_z'   ] = ev.recoSv.z()
         tofill_gen['sv_reco_prob'] = ROOT.TMath.Prob(ev.recoSv.chi2(), int(ev.recoSv.ndof()))
+
+        # for the 2D displacement, take the BS coordinates which are more accurate
+        tofill_gen['hn_reco_2d_disp'] = math.sqrt( (ev.recoSv.x() - ev.beamspot.x(ev.pvs[0].z()))**2 +\
+                                                   (ev.recoSv.y() - ev.beamspot.y(ev.pvs[0].z()))**2  )
+        tofill_gen['hn_reco_3d_disp'] = math.sqrt( (ev.recoSv.x() - ev.beamspot.x(ev.pvs[0].z()))**2 +\
+                                                   (ev.recoSv.y() - ev.beamspot.y(ev.pvs[0].z()))**2 +\
+                                                   (ev.recoSv.z() - ev.pvs[0].z()               )**2  )
 
     ntuple_gen.Fill(array('f',tofill_gen.values()))
 
